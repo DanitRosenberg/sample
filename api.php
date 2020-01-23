032acca0907c08477fbd0fe40b941eeafaf5d19d
Danit Rozenberg Rozenberg <danitrosenber@gmail.com>
2020-01-23 16:54:29 +0200

    second

--- api.php
+++ api.php
@@ -1,4 +1,5 @@
 <?php
+//lesson 112
     session_start();
     $method = $_SERVER["REQUEST_METHOD"];
     switch(strtoupper($method)){
@@ -33,6 +34,11 @@
     function putHandler($json){
         if(hasAction($json,"register")){
             register($json);
+        
+        }else if(hasAction($json,"setArrivedMessages")){
+            updateMessages($json,"ARRIVED");
+        }else if(hasAction($json,"setSeenMessages")){
+            updateMessages($json,"SEEN");
         }else{
             returnError("Unsupported action for this method");
         }
@@ -82,8 +88,27 @@
     }
     
 
-
-
+    function validJsonBody($body){
+        return is_array($body) && isset($body["action"]);
+    }
+    function updateMessages($json,$status){
+        if($userId = getLoggedUserId()){
+            $questionMarks = array_fill(0,count($json["msgsIds"]),"?");
+            $stmt = getDb()->prepare("UPDATE messages SET status = ? WHERE reciever_id = ? AND id IN (".implode(",",$questionMarks).")");
+            if($stmt->execute(array_merge([$status,$userId], $json["msgsIds"]))){
+                return returnSuccess("Messages successfully updated");
+            }
+            returnError("Failed to update messages");
+        }
+        
+    }
+    function getLoggedUserId(){
+        if(isset($_SESSION["userId"])){
+            return $_SESSION["userId"];
+        }
+        return false;
+        
+    }
 
     function responseBody($success, $data){
         return json_encode(["success" => $success, "data" => $data]);
@@ -102,12 +127,55 @@
         header('Content-Type: application/json');
         echo(responseBody(true, $data));
     }
+    function getDb(){
+        $host = "127.0.0.1";
+        $username = "itsafe_user";
+        $password = "123*user*456";
+        $dbName = "itsafe2";
+        return new PDO("mysql:host=$host;dbname=$dbName",$username,$password);
+    }
+
+    function deleteHandler(){
+        logout();
+    }
+    
+    function getHandler(){
+        if($userId = getLoggedUserId()){
+            $db = getDb();
+            $allMessages = [];
+            $stmt = $db->prepare("SELECT m.reciever_id, m.txt ,m.created_at, m.status, u.nickname AS sender_name FROM messages AS m JOIN users AS u ON m.reciever_id = u.id WHERE m.sender_id = ?");
+            $stmt->execute([$userId]);
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $allMessages["msgsFromMe"] = $result;
+            //Get all messages sent to this user
+            $stmt = $db->prepare("SELECT m.sender_id, m.txt, m.created_at, m.status, u.nickname AS sender_name FROM messages AS m JOIN users AS u ON m.sender_id = u.id WHERE m.reciever_id = ?");
+            $stmt->execute([$userId]);
+            $result = $stmt->fatchAll(PDO::FETCH_ASSOC);
+            $allMessages["msgsToMe"] = $result;
+            
+            returnSuccess($allMessages);
+        
+        }else{
+            returnError("User is not logged in");
+        }
+    }
 
 
+    function logout(){
+        unset($_SESSION["userId"]);
+        unset($_SESSION["nickname"]);
+        returnSuccess("logout done");
+    }
 
-
-
-
+    function sendMessage($json){
+        if($userId = getLoggedUserId()){
+            $stmt = getDb()->prepare("INSERT INTO messages(sender_id,receiver_id,txt,status) VALUES (?,?,?,'SENT')");
+            if($r = $stmt->execute([$userId, $json["receiverId"], $json["txt"]])){
+                return returnSuccess("Message is sent");
+            }
+        }
+        returnError("Failed to send messages");
+    }
 
 
 
